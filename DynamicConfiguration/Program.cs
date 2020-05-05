e883f28070ebde65d6a1ebfb7c84d531e0214441
Gurunadh Duvvuru <gurusri.kavi+careerdev@gmail.com>
2020-05-04 17:41:21 -0700

    Checking in DynamicConfiguration project
    Blog:  https://gurunadhduvvuru.wordpress.com/2020/05/04/dynamic-configuration/

---
+++ DynamicConfiguration/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace DynamicConfiguration
+{
+    class Program
+    {
+
+        public static IConfiguration InitConfiguration()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("AppSettings.json")
+                .Build();
+            return config;
+        }
+
+
+        static void Main(string[] args)
+        {
+            IConfiguration Config = Program.InitConfiguration();
+            //Getting config values from JSON configuration file
+            Console.WriteLine(Config["Environments:Dev:Key"]);
+
+            //Getting dynamic config values from JSON configuration file based on Current_ApplicationSettings key, 
+            //Which is currently set to Test environment
+            Console.WriteLine(Config[$"Environments:{Config["ApplicationSettings:Current_ApplicationSettings"]}:Key"]);
+        }
+    }
+}
